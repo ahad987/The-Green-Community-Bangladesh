05471105902cefadbfbb551b421176297d5b9813
Md. Shakil <shakil.eub.cse@gmail.com>
2026-09-06 23:14:13 +0600

    contact form update

--- main/forms.py
+++ main/forms.py
@@ -28,4 +28,8 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ('name', 'email', 'subject', 'message')
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field in self.fields.values():
+                field.widget.attrs.update({ 'class': 'form-control' })
 
